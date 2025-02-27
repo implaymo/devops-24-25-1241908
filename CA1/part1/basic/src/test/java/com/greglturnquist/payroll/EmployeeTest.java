a4b4de961ed1d65c43ebee516c4ca13d7ba8a6a7
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-27 15:21:56 +0000

    Create test shouldCreateEmployeeWithDefaultConstructor for Employee

---
+++ CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -0,0 +1,15 @@
+package com.greglturnquist.payroll;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EmployeeTest {
+
+    @Test
+    void shouldCreateEmployeeWithDefaultConstructor() {
+        // arrange
+        // act
+        // assert
+    }
+  
+}
