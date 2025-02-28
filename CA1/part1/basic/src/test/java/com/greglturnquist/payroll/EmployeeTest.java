9ae3664829dbc420c29ba33a563fd307a25a9c21
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-28 11:25:45 +0000

    test: add jobYears field to shouldThrowIllegalArgumentExceptionWhenSettingEmptyDescription

--- CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
+++ CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -217,7 +217,7 @@ class EmployeeTest {
     @Test
     void shouldThrowIllegalArgumentExceptionWhenSettingEmptyDescription() {
         // arrange
-        Employee employee = new Employee("Antonio", "Silva", "Student");
+        Employee employee = new Employee("Antonio", "Silva", "Student", 1);
         // act & assert
         assertThrows(IllegalArgumentException.class,
                 () -> employee.setDescription(""));
