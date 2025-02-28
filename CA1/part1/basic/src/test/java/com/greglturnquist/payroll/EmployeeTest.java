57f4f9073c93d0bdb249c1bb8ae2de0dcebe9223
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-28 11:24:29 +0000

    test: add jobYears field to shouldThrowIllegalArgumentExceptionWhenSettingFirstNameWithWhitespace

--- CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
+++ CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -172,7 +172,7 @@ class EmployeeTest {
     @Test
     void shouldThrowIllegalArgumentExceptionWhenSettingFirstNameWithWhitespace() {
         // arrange
-        Employee employee = new Employee("Antonio", "Silva", "Student");
+        Employee employee = new Employee("Antonio", "Silva", "Student", 1);
         // act & assert
         assertThrows(IllegalArgumentException.class,
                 () -> employee.setFirstName("   "));
