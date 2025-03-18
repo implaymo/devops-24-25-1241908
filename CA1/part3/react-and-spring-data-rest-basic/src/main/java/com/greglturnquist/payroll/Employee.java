aaa082c75a34dc1b29ac7bb0d7b3621c4ccde015
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-18 18:49:46 +0000

    Add src folder, packega.json and webpack.config.js from basic tutorial. Closes #16

---
+++ CA1/part3/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Entity // <1>
+public class Employee {
+
+	private @Id @GeneratedValue Long id; // <2>
+	private String firstName;
+	private String lastName;
+	private String description;
+	private int jobYears;
+	private String email;
+
+	public Employee() {}
+
+	public Employee(String firstName, String lastName, String description, int jobYears, String email) {
+		if(!isFirstNameValid(firstName)) {
+			throw new IllegalArgumentException("First name can't be null or empty.");
+		}
+		if(!isLastNameValid(lastName)) {
+			throw new IllegalArgumentException("Last name can't be null or empty.");
+		}
+		if(!isDescriptionValid(description)) {
+			throw new IllegalArgumentException("Description can't be null or empty.");
+		}
+		if(!isJobYearsValid(jobYears)) {
+			throw new IllegalArgumentException("Job years can't be negative.");
+		}
+		if(!isEmailValid(email)) {
+			throw new IllegalArgumentException("Email can't be null or empty.");
+		}
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+		this.jobYears = jobYears;
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) &&
+			Objects.equals(firstName, employee.firstName) &&
+			Objects.equals(lastName, employee.lastName) &&
+			Objects.equals(description, employee.description) &&
+			Objects.equals(jobYears, employee.jobYears) &&
+			Objects.equals(email, employee.email);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(id, firstName, lastName, description, jobYears, email);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		if (!isFirstNameValid(firstName)) {
+			throw new IllegalArgumentException("First Name can't be null or empty.");
+		}
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		if (!isLastNameValid(lastName)) {
+			throw new IllegalArgumentException("Last Name can't be null or empty.");
+		}
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		if (!isDescriptionValid(description)) {
+			throw new IllegalArgumentException("Description can't be null or empty.");
+		}
+		this.description = description;
+	}
+
+	public int getJobYears() {
+		return jobYears;
+	}
+
+	public void setJobYears(int jobYears) {
+		if(!isJobYearsValid(jobYears)) {
+			throw new IllegalArgumentException("Job years can't be negative.");
+		}
+		this.jobYears = jobYears;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		if (!isEmailValid(email)) {
+			throw new IllegalArgumentException("Email can't be null or empty.");
+		}
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+			"id=" + id +
+			", firstName='" + firstName + '\'' +
+			", lastName='" + lastName + '\'' +
+			", description='" + description + '\'' +
+			", jobYears=" + jobYears +
+			", email='" + email + '\'' +
+			'}';
+	}
+
+	private boolean isFirstNameValid(String firstName) {
+		return firstName != null && !firstName.trim().isEmpty();
+	}
+
+	private boolean isLastNameValid(String lastName) {
+		return lastName != null && !lastName.trim().isEmpty();
+	}
+
+	private boolean isDescriptionValid(String description) {
+		return description != null && !description.trim().isEmpty();
+	}
+
+	private boolean isJobYearsValid(int jobYears) {
+		return jobYears >= 0;
+	}
+
+	private boolean isEmailValid(String email) {
+		if (email == null || email.trim().isEmpty()) {
+			return false;
+		}
+		String emailRegex = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		return email.matches(emailRegex);
+	}
+
+
+}
+// end::code[]
